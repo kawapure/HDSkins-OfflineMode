c28773d09be49d8ecae3b642d0c5efcb0171b6a8
Sollace <sollacea@gmail.com>
2024-05-05 19:08:24 +0100

    Fix deprecation

--- src/main/java/com/minelittlepony/hdskins/client/modmenu/HDSkinsMenuFactory.java
+++ src/main/java/com/minelittlepony/hdskins/client/modmenu/HDSkinsMenuFactory.java
@@ -1,14 +1,13 @@
 package com.minelittlepony.hdskins.client.modmenu;
 
 import com.minelittlepony.hdskins.client.gui.GuiSkins;
-import com.minelittlepony.hdskins.client.HDSkins;
-
+import com.minelittlepony.hdskins.HDSkinsServer;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 
 public class HDSkinsMenuFactory implements ModMenuApi {
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return s -> GuiSkins.create(s, HDSkins.getInstance().getSkinServerList());
+        return s -> GuiSkins.create(s, HDSkinsServer.getInstance().getServers());
     }
 }
